68954832e5f978e1d1e05aa625df13e68c2f24ec
Roman Sorin <roman@romansorin.com>
2019-11-15 16:34:43 -0500

    Button

--- src/shared/components/AppUIKit/Button/Button.js
+++ src/shared/components/AppUIKit/Button/Button.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Loading } from 'app/ui-kit'
 
 import * as S from './styles'
 
-export default class Button extends Component {
-  static propTypes = {
-    loading: PropTypes.bool
-  }
-
-  render () {
-    const { loading, children } = this.props
-
-    return (
-      <S.Button {...this.props}>
-        <S.ButtonText>
-          <Loading visible={loading} />
-          {children}
-        </S.ButtonText>
-      </S.Button>
-    )
-  }
+const Button = props => {
+  return (
+    <S.Button {...props}>
+      <S.ButtonText>
+        <Loading visible={props.loading} />
+        {props.children}
+      </S.ButtonText>
+    </S.Button>
+  )
 }
+
+Button.propTypes = {
+  loading: PropTypes.bool
+}
+
+export default Button
